18e00638c1149466eda0d14c5cf899d52d08eb31
Maya Roman <maya.roman7@gmail.com>
2020-05-22 18:46:29 -0500

    Update Color Dropdown menu

--- js/script.js
+++ js/script.js
@@ -18,61 +18,48 @@ jobTitles.addEventListener('change', (event) => {
 });
 
 /*
-
 When the form is initially loaded, we need to update the "Design" and "Color" fields so that it's clear to the user that they need to select a theme before selecting a color. Use javaScript to:
-
 */
   
   // ● Hide the “Select Theme” `option` element in the “Design” menu.
   const designMenu = document.querySelector('#design');
   const selectTheme = designMenu.firstElementChild;
-  selectTheme.style.display = 'none';
+  selectTheme.hidden = true;
 
-
-
-  
   // ● Update the “Color” field to read “Please select a T-shirt theme”.
+  const colorMenu = document.querySelector('#color');
+  let colorOptions = colorMenu.options;
+  const firstOption = colorMenu.firstElementChild;
 
-    const colorMenu = document.querySelector('#color');
-    const selectTshirt = document.createElement('option');
-    selectTshirt.textContent = "Please select a T-shirt theme";
-    let selectedOption = colorMenu[colorMenu.selectedIndex];
-    colorMenu.insertBefore(selectTshirt, selectedOption);
-    colorMenu.selectedIndex = 0;
-
-    const colorOptions = document.querySelectorAll('#color option');
-    for (let i = 0; i < colorOptions.length; i++) {
-      colorOptions[i].style.display = 'none';
-    }
-    
-    const showThemeColors = (regex, colors) => {
-      for (let i = 1; i < colors.length; i++) {
-        let colorString = colors[i].textContent;
-        if (regex.test(colorString)) {
-          colors[i].style.display = '';
-        } else {
-          colors[i].style.display = 'none';
-        }
-      }
-    };
-
-    designMenu.addEventListener('change', (event) => {
-      if (event.target.value === 'js puns') {
-        showThemeColors(/JS Puns/, colorOptions);
-      }
-       if (event.target.value === 'heart js') {
-        showThemeColors(/I ♥ JS/, colorOptions);
-      }
-    });
-
-
-    
-    
-
-
-
+  const selectColor = document.createElement('option');
+  selectColor.textContent = "Please select a T-shirt theme";
+  colorMenu.insertBefore(selectColor, firstOption);
+  colorMenu.selectedIndex = 0;
 
   // ● Hide the colors in the “Color” drop down menu.
-  
-  // ● NOTE: Be sure to check out the helpful links in the second section of this Study Guide if you’re unsure of how to accomplish these steps.
+  for (let i = 0; i < colorOptions.length; i++) {
+    colorOptions[i].style.display = 'none';
+  }
+
+  const showThemeColors = (regex, colorOptions) => {
+    for (let i = 0; i < colorOptions.length; i++) {
+      let colorString = colorOptions[i].textContent;
+      if (regex.test(colorString)) {
+        colorOptions[i].style.display = '';
+      } else {
+        colorOptions[i].style.display = 'none';
+      }
+    }
+  };
+
+  designMenu.addEventListener('change', (event) => {
+    if (event.target.value === 'js puns') {
+      colorMenu.selectedIndex = 1;
+      showThemeColors(/JS Puns/, colorOptions);
+    } 
+    if (event.target.value === 'heart js') {
+      colorMenu.selectedIndex = 4;
+      showThemeColors(/I ♥ JS/, colorOptions);
+    } 
+  });
 
